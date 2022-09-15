f597461ad892297888e41c56fa7da7749a488a6c
Jessicasgc <jessicaamadearahma@gmail.com>
2022-09-15 08:12:26 +0700

    view_binding_10789

---
+++ app/src/main/java/com/example/view_binding_10789/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10789
+
+object TaskList {
+    val TaskList = listOf<Task>(
+        Task("Chapter 1","Bangun Tidur","Kebiasaan sehari-hari untuk membuat hari makin indah"),
+        Task("Chapter 2","Nguli Genshin Impact","Mencari Primo buat gacha Mama Yaemiko tercinta / Keqing wangy"),
+        Task("Chapter 3","Ngopi","Habis capek Nguli kan enak ngopi ya kan"),
+        Task("Chapter 4","dimana ei","Buat adu mekanik Showcase Ayaka Pyro"),
+        Task("Chapter 5","Upload ke tiktok","Buat pamer showcase c6 + bennet c6")
+    )
+}
